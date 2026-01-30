759c276e97e43fa38be43b478bfbf568e05aed8d
Adrien <adrien0878@gmail.com>
2026-01-30 10:42:21 +0100

    add db and routes

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-  '', // TODO: database connection string
+  process.env.DATABASE_URL, // Use DATABASE_URL environment variable from Render
   {
     dialect: 'postgres',
     dialectOptions: {
